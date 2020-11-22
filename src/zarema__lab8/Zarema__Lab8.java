b1b6bbfc860d826d90b25ad1409259709cdbd646
zzoasis <54875372+zzoasis@users.noreply.github.com>
2020-11-22 20:01:03 +0600

    initial commit

--- src/zarema__lab8/Zarema__Lab8.java
+++ src/zarema__lab8/Zarema__Lab8.java
@@ -1,7 +1,7 @@
-package zarema__lab6;
+package zarema__lab8;
 
 
-public class Zarema__Lab6 {
+public class Zarema__Lab8 {
 public static double myDiv(double a, double b, double x) throws Exception {
         double y;
          if (x >= 5) {
