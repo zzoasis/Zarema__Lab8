b29a5d8b3131f8437de51ba067c75ffc2045a6ba
zzoasis <54875372+zzoasis@users.noreply.github.com>
2020-11-22 19:59:36 +0600

    initial commit

---
+++ test/zarema_junit/Zarema__Lab8_JUNIT.java
@@ -0,0 +1,30 @@
+package zarema_junit;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+import static zarema__lab8.Zarema__Lab8.myDiv;
+
+public class  Zarema__Lab8_JUNIT {
+    @Test
+    public void test1MyDiv() throws Exception {
+        assertTrue(myDiv(9, 6, 4) == 304);
+        assertTrue(myDiv(8, 7,5) == 565);
+        
+    }
+
+    @Test
+    public void test2MyDiv() throws Exception { 
+        try {
+            assertTrue("Проверка деления 2 на 0", myDiv(2, 0, 0) == 0);
+        } catch (Exception thrown) {
+            assertNotEquals("", thrown.getMessage());
+        }
+        try {
+            assertTrue("Проверка деления 0 на 0", myDiv(0, 0, 0) == 0);
+        } catch (Exception thrown) {
+            assertNotEquals("", thrown.getMessage());
+        }
+    }
+
+}
